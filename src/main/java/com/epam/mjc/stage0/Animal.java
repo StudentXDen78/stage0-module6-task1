84a2e74a5f7c2b3b0da6c6e523bd6d20bf56d37a
xden78 <venera.opt@gmail.com>
2024-10-23 21:42:25 +0300

    1 - animal

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,22 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    String color;
+    int numberOfPaws;
+    boolean hasFur;
 
+    public Animal(String color,  int numberOfPaws, boolean hasFur) {
+        getDescription(color, numberOfPaws,hasFur);
+    }
+
+    public String getDescription(String color, int numberOfPaws, boolean hasFur) {
+        String result;
+        String fur;
+        if (hasFur) {
+            fur = "a";
+        }
+        else fur = "no";
+        result = "This " + "animal is mostly +" + color +". It has + " + numberOfPaws + "paws and " + fur +" fur.";
+        return result;
+    }
 }
